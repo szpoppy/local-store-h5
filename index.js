e0b11000693640f89bbe2c21746571b38cc07a32
张钰峰 <szpoppy@126.com>
2019-09-24 15:01:45 +0800

    首发

---
+++ index.js
@@ -0,0 +1,164 @@
+(function(global, factory) {
+    // UMD 加载方案
+    if (typeof exports === "object" && typeof module !== "undefined") {
+        module.exports = factory();
+        return;
+    }
+    if (typeof define === "function" && define.amd) {
+        define(factory);
+        return;
+    }
+    global.store = factory();
+})(window, function() {
+    "use strict";
+    var soleTime = new Date().getTime() - 1000000;
+    var soleCount = 1000;
+    /**
+     * 获取页面唯一的值
+     * @returns {string}
+     */
+    function getOne() {
+        soleCount;
+        soleCount += 1;
+        return Number(Math.round((Math.random() + 1) * 1000000) + (new Date().getTime() - soleTime) + "" + soleCount).toString(36);
+    }
+
+    // 本地存储引用
+    var LS = window.localStorage;
+    var SS = window.sessionStorage;
+
+    // 测试本地存储是否可以用
+    var canLS = true;
+    try {
+        LS.setItem("ls_can", "1");
+        if (LS.getItem("ls_can") != "1") {
+            canLS = false;
+        }
+    } catch (e) {
+        canLS = false;
+    }
+
+    if (!canLS) {
+        // 无本地存储，模拟本地实现
+        // 数据存储本地，无法长期存储
+        // 缓解无痕模式无法存储的问题
+        var LSStore = {};
+        LS = {
+            getItem(key) {
+                return LSStore[key];
+            },
+            setItem(key, val) {
+                LSStore[key] = val;
+            },
+            removeItem(key) {
+                try {
+                    delete LSStore[key];
+                } catch (e) {}
+            }
+        };
+        var SSStore = {};
+        SS = {
+            getItem(key) {
+                return SSStore[key];
+            },
+            setItem(key, val) {
+                SSStore[key] = val;
+            }
+        };
+    }
+
+    var sKeyKey = ":store-s-key";
+    var sKey = SS.getItem(sKeyKey);
+    if (!sKey) {
+        sKey = getOne();
+        SS.setItem(sKeyKey, sKey);
+    }
+
+    function remove(key) {
+        LS.removeItem(key);
+        return null;
+    }
+
+    return {
+        preposition: ":",
+        /**
+         * 获取本地存储
+         * @param {String} key
+         */
+        getItem: function(key) {
+            key = this.preposition + key;
+            var value = LS.getItem(key);
+            var json = null;
+            try {
+                json = JSON.parse(value);
+            } catch (e) {}
+            if (!json) {
+                // 非
+                return null;
+            }
+
+            // 检测是否过期
+            var expiration = json.koi_expiration;
+            var isOut = false;
+            if (expiration) {
+                var now = new Date().getTime();
+                if (expiration !== -1 && now > expiration) {
+                    // 没过期
+                    isOut = true;
+                }
+            }
+            var session = json.koi_session;
+            if (session && session == sKey) {
+                isOut = true;
+            }
+            if (isOut) {
+                remove(key);
+                return null;
+            }
+            return json.item;
+        },
+        /**
+         * 设置本地存储
+         * @param {String} key
+         * @param {*} value
+         * @param {Number|String|Date} expiration 0:进程，-1:永久，数字:天数，字符串，日期
+         */
+        setItem: function(key, value, expiration) {
+            key = this.preposition + key;
+            var json = { item: value };
+            if (expiration === undefined) {
+                expiration = 0;
+            }
+            var type = typeof expiration;
+            if (type === "number") {
+                if (expiration === 0) {
+                    json.koi_session = sKey;
+                } else if (expiration === -1) {
+                    json.koi_expiration = -1;
+                } else {
+                    json.koi_expiration = new Date().getTime() + expiration * 24 * 60 * 60 * 1000;
+                }
+            } else {
+                // 过期时间 设置为 固定时间
+                if (type === "string") {
+                    expiration = new Date(
+                        expiration
+                            .replace(/\-/g, "/")
+                            .replace(/T/, " ")
+                            .replace(/\.\d*$/, "")
+                    );
+                }
+                json.koi_expiration = expiration.getTime();
+            }
+            LS.setItem(key, JSON.stringify(json));
+        },
+        /**
+         * 移除数据
+         * @param {String} key
+         */
+        removeItem: function(key) {
+            key = this.preposition + key;
+            remove(key);
+        }
+    };
+});
